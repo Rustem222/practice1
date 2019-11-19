af78227c6bb57f5fca3fcea4f7d4d3282a9516b1
Rustem222 <53113123+Rustem222@users.noreply.github.com>
2019-11-19 13:10:04 +0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int x[100][100];
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 8; j++){
+                cin>>x[i][j];
+		}
+	}
+
+return 0;
+}
